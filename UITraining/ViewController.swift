875cddc8467c35516752699f50b7d49a9eb065b4
Ryuzo Yamate <iiiiiii.satenanbon@gmail.com>
2022-02-07 21:58:39 +0900

    initial commit

---
+++ UITraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITraining
+//
+//  Created by r_yamate on 2022/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
